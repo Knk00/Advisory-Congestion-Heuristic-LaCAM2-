6cd2e9ade42a648ad0d0b0036728c779b1485361
Kanishk Soman <kanishkssoman@gmail.com>
2025-06-26 13:34:54 +1000

    Almost done with building the initial data pipeline script for singular examples handoff. Just need to attach the model inference then handover to LaCAM2 dist_table.cpp

--- scripts/real_time_inference_handoff.py
+++ scripts/real_time_inference_handoff.py
@@ -7,87 +7,97 @@ Only for inference purposes, not for training!
 ---
 """
 
-import pandas as pd 
-import numpy as np
-import os
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.utils_advisory_congestion_input import *
+from src.utils_arranging_raw_data import *
 import ast
 from scipy.ndimage import gaussian_filter
 import seaborn as sns
 import matplotlib.pyplot as plt
-import torch
+import torch     
 
-
-def parse_scen_file(fp):
-    scen_df = pd.DataFrame(columns=['unknown_var', 'map', 'map_height', 'map_width',
-                           'start_location_x', 'start_location_y',
-                           'goal_location_x', 'goal_location_y', 'unknown_heuristic'])
-    with open(fp, 'r') as f:
-        scen_file = f.readlines()[1:]
-        
-    for line in scen_file:
-        line = line.strip().split('\t')
-        scen_df.loc[len(scen_df)] = line
-    
-    return scen_df
-
-def preprocess_scen(scen_df, scen_id):
-    scen_df = scen_df.astype({
-          'sif tart_location_x': int,
-          'start_location_y': int,
-          'goal_location_x': int,
-          'goal_location_y': int,
-          'unknown_heuristic': float
-        })
-    scen_df['start_location'] = scen_df.apply(lambda row: (row['start_location_x'], row['start_location_y']), axis=1)
-    scen_df['goal_location'] = scen_df.apply(lambda row: (row['goal_location_x'], row['goal_location_y']), axis=1)
-
-    scen_df['unique_id'] = [scen_id + '_agent_' + str(i+1)  for i in range(len(scen_df))]
-    scen_df.drop(columns=['start_location_x',
-       'start_location_y', 'goal_location_x', 'goal_location_y'], inplace=True)
-    return scen_df
-
-def parse_map(map_file):
-    # Read the map file
-    with open(map_file, 'r') as f:
-        map_data = f.readlines()
-    map_data = map_data[4:]  # Skip the map header
-
-    width = len(map_data[0].strip())  # Infer width from the map
-    height = len(map_data)
-
-    # Initialize the grid representation
-    grid = np.zeros((height, width))
-    for row_idx in range(height):
-        for col_idx in range(width):
-            grid[row_idx, col_idx] = 0 if map_data[row_idx][col_idx] == '.' else 1
 
 
 if __name__ == '__main__':
-    scen_file = '../data/raw/random-32-32-20-even-1.scen' #This scene file can be given as input from bash
     # Bash command to use new scene file:
     # if len(sys.argv) > 1:
     #     scen_file = sys.argv[1]
 
-    #This becomes static input for this map file -- Random 32x32-20 map
-    random_32_32_df = pd.read_csv("../data/raw/random-32-32-20.csv")
-    random_32_32_df['unique_id'] = random_32_32_df['unique_id'].astype(str)
-    grid, wdith, height = parse_map("../data/raw/random-32-32-20/random-32-32-20.map") 
+    # Bash command to select a specific number of agents:
+    # if len(sys.argv) > 1:
+    #     agents = sys.argv[1]
     
+    agents = 50 # This can be set to a specific agent number for testing, e.g., 1, 2, etc.
+    
+    scen_file = './data/raw/random-32-32-20/random-32-32-20-even-25.scen' #This scene file can be given as input from bash
+
+    #This becomes static input for this map file -- Random 32x32-20 map
+    random_32_32_df = pd.read_csv("./data/raw/random-32-32-20/random-32-32-20.csv")
+    random_32_32_df['unique_id'] = random_32_32_df['scen_type'] + '_' + random_32_32_df['type_id'].astype(str) + '_agent_' + random_32_32_df['agents'].astype(str)
+    random_32_32_df['unique_id'] = random_32_32_df['unique_id'].astype(str)
+    
+    grid, wdith, height = parse_map("./data/raw/random-32-32-20/random-32-32-20.map") 
+    
+    # Parse the scenario file
+    # This function reads the scenario file and returns a DataFrame with relevant columns.
     scen_df = parse_scen_file(scen_file)
-    scen_df = preprocess_scen(scen_df, os.path.basename(scen_file).split('.')[0])
-    scen_df['unique_id'] = scen_df['unique_id'].astype(str)
+    scen_id = scen_file.split('random-32-32-20-')[-1].split('.scen')[0]
+    scen_id = scen_id.replace('-', '_')
+    scen_df = preprocess_scen(scen_df, scen_id)
+    scen_df = scen_df.iloc[:agents] # Limit to the specified number of agents
 
-    # Only keep rows from random_32_32_df that match scen_df['unique_id']
-    merged_df = pd.merge(
-        scen_df,
-        random_32_32_df,
-        on='unique_id',
-        how='left'
-    )
+    # Create a reference DataFrame for start and goal locations
+    # This DataFrame will be used to match the agents in the scenario file with the agents
+    ref_loc_df = scen_df[['unique_id', 'start_location', 'goal_location']].copy()
+    ref_loc_df['start_location'] = ref_loc_df['start_location'].astype(str) 
+    ref_loc_df['goal_location'] = ref_loc_df['goal_location'].astype(str)
 
-    ref_loc_df = merged_df[['scen_type', 'type_id', 'agents', 'start_location', 'goal_location']]
     ref_loc_df['start_location'] = ref_loc_df['start_location'].apply(ast.literal_eval)
     ref_loc_df['goal_location'] = ref_loc_df['goal_location'].apply(ast.literal_eval)
 
+
+    #Filter the random_32_32_df to only include unique_ids present in the scenario file
+    # This ensures that we only work with the agents defined in the scenario file.
+    random_32_32_df = random_32_32_df[random_32_32_df['unique_id'].isin(ref_loc_df['unique_id'])].iloc[-1]
+
     # Model input data
-    merged_df['makespan'] = [len(max(i.split('\n'), key=len)) for i in merged_df['solution_plan']]
+    makespan = [len(max(i.split('\n'), key=len)) for i in random_32_32_df['solution_plan']]
+
+    # Transpose the grid for aligning with standard positional indexing
+    # This is necessary because the grid is represented in a way that matches the scenario file.
+    grid_1 = grid.T.copy()
+    grid_2 = grid.T.copy()
+    simulation_grid = grid.T.copy()
+
+    # Extract solution plans from the DataFrame
+    solution_plan = random_32_32_df['solution_plan']
+
+    # def compute_frequency_heatmap(solution_plan, makespan, ref_loc_df):
+    start_locations = []
+    goal_locations = []
+
+    query = ref_loc_df.iloc[agents - 1].copy()
+    
+    start_loc = tuple(query['start_location'])
+    start_locations.append(start_loc)
+
+    goal_loc = tuple(query['goal_location'])
+    goal_locations.append(goal_loc) 
+
+    for timestep in range(len(makespan)):
+        curr_pos = calculate_curr_position(timestep, solution_plan[agents - 1], start_loc)
+
+        # Ensure position is within bounds
+        if 0 <= curr_pos[0] < simulation_grid.shape[0] and 0 <= curr_pos[1] < simulation_grid.shape[1]:
+            if simulation_grid[curr_pos] >= 0:
+                simulation_grid[curr_pos] += 1
+            else:
+                skip_counter += 1
+
+        # Stop if agent reaches goal exactly at the end
+        if timestep == len(solution_plan[agent - 1]):
+            if tuple(query['goal_location'].values[0]) == curr_pos:
+                break
+
+
